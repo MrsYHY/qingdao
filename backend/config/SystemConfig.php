dca9bea30fb0a1133a5cdc321eb9ffcb85d5b4ea
ImKouga <158664981@qq.com>
2016-04-17 21:45:02 +0800

    init

---
+++ backend/config/SystemConfig.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 系统配置文件
+ * @author kouga-huang
+ * @since 2015-11-8
+ *
+ */
+namespace backend\config;
+
+use common\config\BaseConfig;
+
+class SystemConfig extends BaseConfig{
+
+    const WEB_SITE = "http://hikouga.local";
+}
