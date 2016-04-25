417028101276588dcfc38177623a58fceadded72
cj@feibo.cn <cj@feibo.cn>
2016-04-25 13:07:21 +0800

    init

--- backend/views/themes/adminlte/wc-site/qr_code.php
+++ backend/views/themes/adminlte/wc-site/qr_code.php
@@ -13,7 +13,7 @@ $musicPath = $this->getAssetManager()->publish('@wechat/music/')[1];
 <img src="<?=$imgPath?>/comfirm_2.png" width="100%" height="100%" style="position:absolute; left:0; top: 0; z-index: -1;">
 <img src="<?=$imgPath?>/comfirm_3.png" width="100%" height="100%" style="position:absolute; left:0; top: 0; z-index: -1;">
 <div id="info" class="row" style="margin-top:20px;height: 50px">
-    <img style="position: absolute;top: 20%;left: 25%;" src="<?=$img?>">
+    <img style="position: absolute;top: 25%;left: 25%;" src="<?=$img?>">
 </div>
 
 <div class="row" style="text-align: center;   position: absolute; bottom: 17%; margin-bottom: 10px; left: 50%; margin-left: -100px; z-index: 998;">
